80b15f4740645087d89bf85a50d7bb55a097fd37
Thuraya <hallo@example.com>
2024-05-13 22:08:50 +0200

    Datei hallo.py  wurde implementiert

--- hallo.py
+++ hallo.py
@@ -4,3 +4,4 @@ y = 4
 # git config --global user.email "hallo@example.com"
 # git config --global user.name "Thuraya"
 z = 100
+# git config --global http.proxy <u><link=http://proxywbs:3128>http://proxywbs:3128</link></u>
